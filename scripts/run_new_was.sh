74b859ce7ae008a5b48eca52a0628388b656db2b
LminWoo99 <mw310@naver.com>
2023-09-28 16:53:43 +0900

    change run

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -22,6 +22,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /etc/server/PlantBackend/build/libs/* > /etc/server/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /etc/server/PlantBackend/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0

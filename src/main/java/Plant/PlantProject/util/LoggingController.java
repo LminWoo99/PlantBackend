3a9a912deb80b7c610f4eef7a4c14e2f3077c28a
LminWoo99 <mw310@naver.com>
2023-09-28 17:09:16 +0900

    change endpoint

--- src/main/java/Plant/PlantProject/util/LoggingController.java
+++ src/main/java/Plant/PlantProject/util/LoggingController.java
@@ -4,18 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
-
+@RequestMapping("/api")
 public class LoggingController {
     @Value("${logging-module.version}")
     private String version;
 
-    @GetMapping("/")
+    @GetMapping("/version")
     public String version() {
         return String.format("Project Version : %s", version);
     }

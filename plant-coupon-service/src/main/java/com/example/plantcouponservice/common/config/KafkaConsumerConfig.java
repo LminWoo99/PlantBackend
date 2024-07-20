9c7e83c001051e9421f60ab40ea908d88dbe01fe
LminWoo99 <mw310@naver.com>
2024-07-20 16:58:59 +0900

    fix : consumer config 수정

--- plant-coupon-service/src/main/java/com/example/plantcouponservice/common/config/KafkaConsumerConfig.java
+++ plant-coupon-service/src/main/java/com/example/plantcouponservice/common/config/KafkaConsumerConfig.java
@@ -14,6 +14,7 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
+import java.util.HashMap;
 import java.util.Map;
 @EnableKafka
 @Configuration
@@ -69,6 +70,27 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, PaymentRequestDto> couponUseListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, PaymentRequestDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(couponUseConsumerFactory());
+
+        return factory;
+
+    }
+    @Bean
+    public ConsumerFactory<String, String> paymentFailedConsumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServerUrl);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "payment");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(config);
+
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> paymentFailedListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(paymentFailedConsumerFactory());
 
         return factory;
 
